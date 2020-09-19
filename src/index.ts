bb18f4cbefc1d72373c8d495481b68d127210aba
fun4wut <nihouze@163.com>
2020-09-19 23:52:09 +0800

    rua

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import SA from 'superagent'
-import { createWorker, recognize } from 'tesseract.js'
+import { createWorker,  } from 'tesseract.js'
 
 import encode from './des'
 import images from 'images'
@@ -17,9 +17,8 @@ declare module 'superagent' {
 // install the extension
 export function install(request: SA.SuperAgentStatic) {
   request.log2ECNU = async function(username: string, password: string) {
-    
     const res = await request.get(LOGIN_URL)
-    
+
     let ltReg = /id="lt" name="lt" value="(.*?)" \/>/
     let executionReg = /name="execution" value="(.*?)" \/>/
 
@@ -27,12 +26,14 @@ export function install(request: SA.SuperAgentStatic) {
     const execution = executionReg.exec(res.text)![1]
     const rsa = encode(username + password + lt, '1', '2', '3')
 
-    const pic = await request.get(PIC_URL)
+    const pic = await request
+      .get(PIC_URL)
       .buffer(true)
       .parse(SA.parse.image)
     const code = await recognizeImg(pic.body)
 
-    const res2 = await request.post(LOGIN_URL)
+    const res2 = await request
+      .post(LOGIN_URL)
       .type('application/x-www-form-urlencoded')
       .send({
         code,
@@ -43,7 +44,8 @@ export function install(request: SA.SuperAgentStatic) {
         lt,
         execution,
         _eventId: 'submit',
-      }).catch(e => {
+      })
+      .catch(e => {
         console.error(e.response.text)
         throw e
       })
@@ -56,7 +58,7 @@ async function recognizeImg(buf: Buffer) {
   const worker = createWorker({
     // self OSS
     langPath: 'https://gofun4-pic.oss-cn-hangzhou.aliyuncs.com',
-    gzip: false
+    gzip: false,
   })
   await worker.load()
   await worker.loadLanguage('digits_layer')
